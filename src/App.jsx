beea1d7e6fb406eb5c92250ff2cd5377973b961a
Linus Ilbratt <linusilbratt@gmail.com>
2024-09-26 12:25:32 +0200

    replaced navbar with sidebar and turned home.jsx into a dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar'; // Replace Navbar with Sidebar
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import DogDetails from './pages/DogDetails';
 import RegisterDog from './pages/RegisterDog';
-import './App.css'
+import './App.css';
 
 function App() {
   const [dogs, setDogs] = useState([]);
 
-  
   useEffect(() => {
-    
     const fetchDogs = async () => {
       try {
         const response = await fetch('https://api.jsonbin.io/v3/b/66ea8f94ad19ca34f8a82e28');
@@ -32,17 +30,19 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/catalog' element={<Catalog dogs={dogs} />} />
-          <Route path='/dog/:chipNumber' element={<DogDetails dogs={dogs} />} />
-          <Route path='/register' element={<RegisterDog />} />
-        </Routes>
+        <Sidebar />  {/* Include Sidebar */}
+        <div className='main-content'>
+          {/* Wrap your routes in the main-content div */}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/catalog' element={<Catalog dogs={dogs} />} />
+            <Route path='/dog/:chipNumber' element={<DogDetails dogs={dogs} />} />
+            <Route path='/register' element={<RegisterDog />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
 }
 
 export default App;
-
