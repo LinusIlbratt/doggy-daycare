b42599b55fe228aa6ceab2358720bdaec056d19b
Linus Ilbratt <linusilbratt@gmail.com>
2024-09-27 09:32:57 +0200

    function to change the height dynamic for iphone/safari

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import pawLogo from '../assets/pawlogo.png';
 import { FaHome, FaRegFileAlt, FaDog, FaExclamationTriangle } from 'react-icons/fa'; 
 import './Sidebar.css'; 
 
 function Sidebar() {
+
+  useEffect(() => {
+    // Function to handle dynamic viewport height
+    function handleResize() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    // Set initial height and listen for resize events
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty dependency array to run only on mount and unmount
+
   return (
-    <div className="sidebar">      
+    <div className="sidebar" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>      
       <ul className="sidebar-links">
         <li>
           <Link to="/">
